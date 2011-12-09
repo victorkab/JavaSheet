dfa4dea603ec29d2abca9f5dc004b1f86f57cc4e
Victor Kabdebon <victor@victorkabdebon.com>
2011-12-09 18:47:40 -0500

    Adding core JavaSheet file

---
+++ src/javasheet.core.js
@@ -0,0 +1,23 @@
+/**
+ * Description: This
+ * 
+ * Author:
+ * Website:
+ * 
+ * 
+ */
+
+
+(function(){
+     var JavaSheet={
+	 elems:[]
+     };
+     
+     /**
+      * Creating shortcut for javasheet
+      */
+     if(!window.jsheet){
+	 window.jsheet=JavaSheet;
+     }
+     
+ })();
