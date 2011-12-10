8b70edd674ada411d592dbbd61d9961af08cbd0e
Victor Kabdebon <victor@victorkabdebon.com>
2011-12-09 20:16:37 -0500

    Adding bash script

---
+++ jsdump.sh
@@ -0,0 +1,9 @@
+# Just a bash script to move the javascript file from src to tests in order to run them in an html page
+# WARNING IT WILL ERASE FILE IN tests !
+#!/bin/bash
+#
+if [ ! -d "tests/js" ]; then
+    echo "Creating js directory"
+    mkdir tests/js
+fi
+cp src/*.js tests/js
